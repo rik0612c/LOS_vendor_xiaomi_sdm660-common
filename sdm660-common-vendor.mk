0dae01dbe5c13afc03dea41f6a38f1ffdf25888a
rik0612c <qq872638399@gmail.com>
2023-04-08 16:18:21 +0800

    sdm660-common: Drop QTI datastatusnotification

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -482,7 +482,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
